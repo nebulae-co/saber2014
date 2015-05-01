07e04e9288a30b37760a74bfe33436da8cdbf651
daniel <damorenoco@unal.edu.co>
2015-05-01 15:58:01 -0500

    Fix download method.

--- data/data.R
+++ data/data.R
@@ -1,11 +1,15 @@
 # Paquetes
 
 url <- "https://github.com/finiterank/saber2014/raw/master/tabla.saber.tar.gz"
-file <- "tabla.saber."
-download.file(url = url, destfile = paste0(file, "tar.gz"))
+file <- "tabla.saber.tar.gz"
+method <- if(capabilities("libcurl")) "libcurl" else "auto"
+download.file(url = url, destfile = file, mode = "wb", method = method)
 untar(file)
 
-Saber11 <- read.csv(file = paste0(file, "2014.csv"))
+file <- sub(pattern = "tar.gz", replacement = "2014.csv", 
+            x = file, fixed = TRUE)
+
+Saber11 <- read.csv(file = file)
 
 # head(Saber11)
 # dim(Saber11)
